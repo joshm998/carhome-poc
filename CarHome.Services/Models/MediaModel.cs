8916f93edf548a9e9274cda2f13897500af98cf0
Joshua Mangiola <joshm982@gmail.com>
2021-12-28 11:25:03 +1030

    Adds MediaModel

---
+++ CarHome.Services/Models/MediaModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHome.Services.Models
+{
+    internal class MediaModel
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public MediaStatus MediaStatus { get; set; }
+        public int Duration { get; set; }
+        public int Position { get; set; }
+    }
+
+    public enum MediaStatus
+    {
+        Playing,
+        Paused,
+    }
+}
